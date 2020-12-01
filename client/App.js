a9939a65bde04a9234b0f6fd3fbffbade6c751c1
betalfa <trymhm@gmail.com>
2020-12-01 18:52:40 +0100

    PushToThomas

--- client/App.js
+++ client/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Routes from "./Components/Routes/Routes"
+import Routes from "./src/utils/Components/Routes/Routes"
 import {BrowserRouter as Router}  from 'react-router-dom';
 
 
--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ThemeProvider, CSSReset } from '@chakra-ui/core';
-import customTheme from './src/styles/theme';
 import Routes from './src/routes/Routes';
 
 const App = () => (
