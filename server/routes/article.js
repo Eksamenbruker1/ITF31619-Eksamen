f94aa0f73b727f8135f456bd56de9d5172593001
Eksamenbruker1 <75315801+Eksamenbruker1@users.noreply.github.com>
2020-12-09 00:16:03 +0100

    Temporarily disabled auth checks on articles

--- server/routes/article.js
+++ server/routes/article.js
@@ -7,8 +7,9 @@ const router = express.Router();
 router.get("/:slug", articleController.get);
 router.get("/", articleController.list);
 
+//router.post("/", [isAuthenticated, isAuthorized()], articleController.create);
 router.post("/", [isAuthenticated, isAuthorized()], articleController.create);
-
+//router.put("/:id", [isAuthenticated, isAuthorized()], articleController.update);
 router.put("/:id", [isAuthenticated, isAuthorized()], articleController.update);
 
 router.delete(
